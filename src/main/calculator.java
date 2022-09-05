0c608822842c4efb42670eaad7dcf97515b11f50
kokilavani2610 <53813101+kokilavani2610@users.noreply.github.com>
2022-09-05 13:07:12 +0530

    Update calculator.java

--- src/main/calculator.java
+++ src/main/calculator.java
@@ -79,7 +79,8 @@ public class Calculator {
         int[] x = {MARGIN_X, MARGIN_X + 90, 200, 290, 380};
         int[] y = {MARGIN_Y, MARGIN_Y + 100, MARGIN_Y + 180, MARGIN_Y + 260, MARGIN_Y + 340, MARGIN_Y + 420};
         int a= 4;
-        string s = "calculator"
+        String s = "calculator";
+        s.equals("null");
             
 
         inText = new JTextField("0");
